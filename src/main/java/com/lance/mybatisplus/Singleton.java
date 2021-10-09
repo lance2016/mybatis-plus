a02afa2d65c13b805447aac00d75b9f289758a98
chenfeilong <chenfeilong@cloudwalk.com>
2021-10-09 17:19:44 +0800

    移除<package>pom</package>，修复启动失败问题；添加初始化线程池及使用信号量示例

--- src/main/java/com/lance/mybatisplus/Singleton.java
+++ src/main/java/com/lance/mybatisplus/Singleton.java
@@ -1,5 +1,12 @@
 package com.lance.mybatisplus;
 
+import io.netty.util.concurrent.DefaultThreadFactory;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @program: mybatisplus  Singleton
  * @description:
@@ -26,6 +33,29 @@ public class Singleton {
     }
 
     public static void main(String[] args) {
-        new Thread(() -> System.out.println(Singleton.getInstance())).start();
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(10,10,0, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new DefaultThreadFactory("thread-poll-"), new ThreadPoolExecutor.AbortPolicy());
+        Semaphore semaphore = new Semaphore(5);
+        for(int i=0;i<10;i++){
+            executor.submit(()->{
+                if(semaphore.availablePermits()==0){
+                    System.out.println("资源不足，请耐心等待");
+                }
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "获取令牌");
+                    Thread.sleep(1000);
+                    Singleton singleton = Singleton.getInstance();
+                    System.out.println(singleton.hashCode());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                    System.out.println(Thread.currentThread().getName() + "释放资源");
+                }
+
+
+            });
+        }
+        executor.shutdown();
     }
 }
